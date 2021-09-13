56b50ec725a1bb26bd3690121ab7a48656c6a696
ruelngatia <ruelngatia@users.noreply.github.com>
2021-09-13 17:48:44 +0300

    first commit

---
+++ ussd.php
@@ -0,0 +1,14 @@
+<?php
+
+	$sessionID = $_POST['sessionId'];
+	$serviceCode = $_POST['serviceCode'];
+	$phoneNumber = $_POST['phoneNumber'];
+	$text = $_POST['text'];
+
+	if (text = '') {
+		$response = "CON this is one"
+	}else{
+		$response ="nonsense"
+	}
+
+?>
